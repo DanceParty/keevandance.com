f5e8971faf4d2aed75e784ccd59a56dd1c0a85a5
Keevan Dance <danceparty@github.com>
2024-01-23 16:03:27 +0900

    Fix TwitterIcon TS error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import vercelStatic from "@astrojs/vercel/static";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: "static",
+  adapter: vercelStatic({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  integrations: [tailwind()],
 });
