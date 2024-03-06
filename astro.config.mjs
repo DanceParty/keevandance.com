ebeb16f5be335828c4ff3322e7f7804fc43063b5
Keevan Dance <danceparty@github.com>
2024-03-06 12:28:05 +0900

    Move consistent styling to global.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 import vercelStatic from "@astrojs/vercel/static";
 import tailwind from "@astrojs/tailwind";
+import postcssImport from "postcss-import";
+import postcssNesting from "tailwindcss/nesting";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,4 +15,11 @@ export default defineConfig({
     },
   }),
   integrations: [tailwind()],
+  vite: {
+    css: {
+      postcss: {
+        plugins: [postcssImport, postcssNesting, tailwindcss, autoprefixer],
+      },
+    },
+  },
 });
